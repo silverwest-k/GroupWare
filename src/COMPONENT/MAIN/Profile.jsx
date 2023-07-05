f96c9b9b50851ccb00d87247db924ebf40879944
Eunseo <supreme327@naver.com>
2023-07-05 20:26:09 +0900

    로그인페이지 화면수정

--- src/COMPONENT/MAIN/Profile.jsx
+++ src/COMPONENT/MAIN/Profile.jsx
@@ -1,4 +1,5 @@
 import styles from "./Profile.module.css"
+import {Link} from "react-router-dom";
 
 function Profile() {
     return(
@@ -12,7 +13,7 @@ function Profile() {
                     <p>장그래 사원</p>
                     <p>부서 : 영업3팀</p>
                     <p>사번 : 213327</p>
-                    <p className={styles.logOut}>로그아웃</p>
+                    <Link to="/login"><p className={styles.logOut}>로그아웃</p></Link>
                 </div>
             </div>
         </div>
--- src/PAGES/LogIn.jsx
+++ src/PAGES/LogIn.jsx
@@ -1,36 +1,71 @@
 import axios from "axios";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import styles from "./Login.module.css"
+import {Alert} from "react-bootstrap";
 
 
 function LogIn() {
-    const [userId, setUserId] = useState("")
-    const [userPassword, setUserPassword] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [showAlert, setShowAlert] = useState(false);
 
-    const login = ()=> {
-        axios.post("http://172.20.10.8:9091/members/login", {
-            mid: userId,
-            password: userPassword
+    const submit = ()=> {
+        axios.post("http://172.20.10.8:9091/auth/login", {
+            "email": email,
+            "password": password
         })
-            .then(function (res) {
-                console.log(res);
+            .then((res) => {
+                console.log(res.data);
             })
-            .catch(function (error){
+            .catch((error) =>{
                 console.log(error);
             })
     }
 
+    const loginCheck = () => {
+        if (email.trim() === '') {
+            setShowAlert(true);
+        }
+    }
+
     return(
-        <>
-            id <input value={userId}
-                      onChange={(e)=>{setUserId(e.currentTarget.value)}}
-            />
+        <div className={styles.wrapper}>
+            <div className={styles.loginContainer}>
+                {/*<form action="/">*/}
+                <h2>ACCOUNT LOGIN</h2>
+                <div className={styles.inputWrap}>
+                    <div className="my-3">
+                       <input type="text" className="form-control" id="email"
+                              placeholder="사번"
+                              value={email}
+                              onChange={(e)=>{setEmail(e.currentTarget.value)}}
+                    />
+                    </div>
+                    <div className="my-3">
+                       <input type="password" className="form-control" id="password"
+                              placeholder="비밀번호"
+                              value={password}
+                              onChange={(e)=>{setPassword(e.currentTarget.value)}}
+                    />
+                    </div>
+                    <button type="submit" className={`${styles.button} btn btn-primary`} id="send"
+                            onClick={() => {
+                                submit();
+                                loginCheck();
+                            }}
+                    >로그인</button>
+                </div>
+                {/*</form>*/}
+            </div>
 
-            pw <input value={userPassword}
-                      onChange={(e)=>{setUserPassword(e.currentTarget.value)}}
-            />
-
-            <button onClick={login}>로그인</button>
-        </>
+            {showAlert && (
+                <div className={styles.alert}>
+                    <Alert variant="danger">
+                        아이디를 입력하세요!
+                    </Alert>
+                </div>
+            )}
+        </div>
     )
 }
 
